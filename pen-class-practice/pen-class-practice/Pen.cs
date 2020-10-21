66307e9a2281a7e35d25d6c33f6c582a1e03d9b0
Krisztina Pap <krisztina.pap@outlook.com>
2020-10-21 11:35:06 -0600

    Adds Pen class with properties;

---
+++ pen-class-practice/pen-class-practice/Pen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pen_class_practice
+{
+    class Pen
+    {
+        public string Brand { get; set; }
+
+        public string Colour { get; set; }
+
+        private double _inkLevel;
+        public double InkLevel 
+        {
+            get
+            {
+                return _inkLevel;
+            }
+            set
+            {
+                if (value < 0 || value > MaxInk)
+                {
+                    throw new Exception("Ink level must be between zero and maximum ink level.");
+                }
+                _inkLevel = value;
+            }
+        }
+
+        public double MaxInk { get; set; }
+
+        public bool HasInk { get; set; }
+
+    }
+}
